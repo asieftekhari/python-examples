ca5a7c9e206a39e20f72e4b1a5f69a552ec00a18
Asieh Eftekhari <a.eftekhari2011@gmail.com>
2026-06-11 00:18:42 +0330

    Add files via upload

---
+++ Example_1.py
@@ -0,0 +1,9 @@
+x=float(input("Enter the first number: "))
+y=float(input("Enter the second number: "))
+
+print(f"{x} + {y}  = ", "%.3f"%(x+y))
+print(f"{x} - {y}  = ", "%.3f"%(x-y))
+print(f"{x} * {y}  = ", "%.3f"%(x*y))
+print(f"{x} / {y}  = ", "%.3f"%(x/y))
+print(f"{x} ** {y} = ", "%.3f"%(x**y))
+print(f"{x} // {y} = ", "%.3f"%(x//y))
